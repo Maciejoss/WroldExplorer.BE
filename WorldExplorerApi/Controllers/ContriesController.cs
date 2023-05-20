ee26bc868b7fa9a796a8422b230416c1f9a96d1e
Maciejoss <maciejkordyaczny@gmail.com>
2023-05-20 09:23:08 +0200

    porject setup

---
+++ WorldExplorerApi/Controllers/ContriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorldExplorerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContriesController : ControllerBase
+    {
+        // GET: api/<ContriesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ContriesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
